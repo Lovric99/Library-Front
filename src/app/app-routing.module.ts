70cedfe2dcd26a65b9ed212ac63ab5ee6df374da
Lovric99 <lovric.luka999@gmail.com>
2023-05-04 19:56:16 +0200

    new component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 import { HomeComponent } from './core/home/home.component';
 import { LogInComponent } from './log-in/log-in.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
-  {path: 'log', component: LogInComponent}
+  {path: 'log', component: LogInComponent},
+  {path: 'books', component: BooksComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LogInComponent } from './log-in/log-in.component';
+import { SideBarComponent } from './core/side-bar/side-bar.component';
+import { HomeComponent } from './core/home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BooksComponent } from './books/books.component';
+import { CommonModule } from '@angular/common';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    SideBarComponent,
+    HomeComponent,
+    BooksComponent,
+    BookDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
